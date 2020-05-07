057d1167383d96ea648dd6f6c4042520a1eb44ed
unknown <C:\Users\Zelich\AppData\Roaming\The Bat!>
2020-05-07 14:03:52 +0300

    7/05 14:03

--- js/action.js
+++ js/action.js
@@ -13,3 +13,8 @@ $(".service__content .service__specific").click(function() {
     $(".service__content .service__specific").removeClass("active").eq($(this).index()).addClass("active");
     $(".service__item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
+
+
+$(".js-map-show").click(function() {
+    $(".js-map-show").toggleClass("active");
+});
